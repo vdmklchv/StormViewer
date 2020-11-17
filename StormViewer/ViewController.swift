3e69a770e9967e85139475443506ca272c788da7
Vadim Colcev <vadimkolchev@gmail.com>
2020-11-17 20:10:23 +0200

    Initial Commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Vadim Colcev on 11/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
